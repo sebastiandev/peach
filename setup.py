1ab3323914c09d1eb78703aa231e4990977d6a22
seba <seba@localhost.localdomain>
2017-04-28 15:35:17 -0300

    setup files for pypi

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='peach',
+    packages=['peach'],
+    version='0.1',
+    description='A Lightweight framework for building restful apis on top of flask',
+    author='Sebastian Packmann',
+    author_email='devsebas@gmail.com',
+    url='https://github.com/sebastiandev/peach',
+    download_url='https://github.com/sebastiandev/peach/archive/0.1.tar.gz',
+    keywords=['flask', 'python', 'api', 'framework', 'restful'],
+    classifiers=[],
+)
